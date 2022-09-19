f205c50e5bb73151ee9288d7526b95b5d4fcbbbe
Roman Koval <romankoval10@gmail.com>
2022-09-19 15:55:08 -0400

    init

---
+++ KeyMe/KeyMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyMe
+//
+//  Created by Roman Koval on 2022-09-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeyMe/KeyMe/KeyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyMeApp.swift
+//  KeyMe
+//
+//  Created by Roman Koval on 2022-09-19.
+//
+
+import SwiftUI
+
+@main
+struct KeyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
