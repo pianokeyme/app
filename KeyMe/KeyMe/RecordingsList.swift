067eae315eba062ff52b0c9f7db39818e3b6d44c
Youssef Elfaramawy <yelfaram@gmail.com>
2022-11-27 22:11:13 -0500

    added playback rate functionality

--- KeyMe/KeyMe/RecordingsList.swift
+++ KeyMe/KeyMe/RecordingsList.swift
@@ -16,7 +16,7 @@ struct RecordingsList: View {
     
     let audioRecorder = AudioRecorder()
     
-    var recordings = [Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav")!, createdAt: Date())]
+    var recordings = [Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav")!, createdAt: Date()), Recording(fileURL: URL(string: "https://www2.cs.uic.edu/~i101/SoundFiles/Fanfare60.wav")!, createdAt: Date())]
 
     var body: some View {
         List {
@@ -53,7 +53,9 @@ struct RecordingsList: View {
 
 struct RecordBottomSheet: View {
     @State var isPlaying: Bool
-    let audioRecorder = AudioRecorder()
+    @State var selectedRate = "1.0"
+    
+    var playbackRates = ["0.25", "0.5", "0.75", "1.0", "1.25", "1.5", "1.75", "2.0"]
     
     init(isRecord: Bool) {
         self.isPlaying = isRecord
@@ -73,6 +75,16 @@ struct RecordBottomSheet: View {
                 .overlay(
                     Circle().stroke(Color(red: 143/255, green: 0, blue: 26/255), lineWidth: 4))
         }
+        Picker("Playback Rate", selection: $selectedRate) {
+            ForEach(playbackRates, id: \.self) {
+                Text($0)
+            }.padding(.vertical)
+        }
+        .onChange(of: selectedRate, perform: { _ in
+            setPlayerRate(player: audioPlayer, rate: Float(selectedRate)!)
+            audioPlayer.playImmediately(atRate: Float(selectedRate)!)
+        })
+        .pickerStyle(.wheel)
     }
 }
 
@@ -100,6 +112,24 @@ struct Composition: Codable, Hashable, Identifiable {
     var analyzed_id: Int
     var created_by: Int
     var created_at: Date
+}
+
+func setPlayerRate(player: AVPlayer, rate: Float) {
+    // AVFoundation wants us to do most things on the main queue.
+    DispatchQueue.main.async {
+        if (rate == player.rate) {
+            return
+        }
+        if (rate > 2.0 || rate < -2.0) {
+            let playerItem = player.currentItem
+            player.replaceCurrentItem(with: nil)
+            player.replaceCurrentItem(with: playerItem)
+            player.rate = rate
+        } else {
+            // No problems "out of the box" with rates in the range [-2.0,2.0].
+            player.rate = rate
+        }
+    }
 }
 
 @available(iOS 16.0, *)
